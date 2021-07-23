30ef7df9e87a9b0668a5aa3e08ff2d84e770361e
Kamesh V R <67401750+kamesh15@users.noreply.github.com>
2021-07-23 11:35:12 +0530

    Update Repository.java

--- Task3/Service/Repository.java
+++ Task3/Service/Repository.java
@@ -8,14 +8,14 @@ public class Repository {
 		
 		String con=null;
 		int cc=Integer.parseInt(countryCode);
-		String cc1=countryCode;
+		
 		if(cc>=70&&cc<=99)
 			 con="India";
 		else if(cc==908)
 			 con="US";
 		else {	
 	          try {
-		            if(cc1=="011")
+		            if(CountryCode.equals("011"))
 			           {throw new countryNotfoundException("Dial somewhere outside of USA");}
 		            else
 			           {throw new InvalidCodeException("No country with given code found");}
